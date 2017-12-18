3fafa7e000302c346dff39aaa88b6819912881bc
hu970804 <812510003@qq.com>
2017-12-18 19:26:18 +0800

    添加github登录注销功能

--- routes/auth.js
+++ routes/auth.js
@@ -10,12 +10,40 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(obj, done) {
-  console.log('---deserializeUser---')
+  console.log('---deserializeUser---');
+  console.log(obj);
   done(null, obj);
 });
 
-router.get('/github',function(req, res, next) {
+passport.use(new GitHubStrategy({
+  clientID: '64bccbc422247855a20e',
+  clientSecret: 'cf5725fd37449c83ec231d81da4a78ba49ae1b97',
+  callbackURL: "http://localhost:3000/auth/github/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  done(null, profile);
+}
+));
 
+router.get('/logout', function(req, res){
+  req.session.destroy();
+  res.redirect('/');
+})
+
+router.get('/github', passport.authenticate('github'));
+
+router.get('/github/callback',
+passport.authenticate('github', { failureRedirect: '/login' }),
+function(req, res) {
+  console.log('success....');
+  console.log(req.user);
+  req.session.user = {
+    id: req.user.id,
+    username: req.user.displayName || req.user.username,
+    avatar: req.user._json.avatar_url,
+    provider: req.user.provider
+  };
+  res.redirect('/');
 });
 
 module.exports = router;
